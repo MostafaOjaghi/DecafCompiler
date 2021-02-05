8450d604b3825cddafd03339e1ac8566f29721ce
hrzuser <hamidrezakamkari@gmail.com>
2021-02-05 00:53:13 -0800

    namespaces created

--- src/SyntaxTree/Node.h
+++ src/SyntaxTree/Node.h
@@ -7,22 +7,22 @@
 
 
 #include <vector>
+namespace SyntaxTree {
+    class Decl;
 
-class Decl;
+    class Node {
 
-class Node {
+    public:
+        virtual void cgen() = 0;
+    };
 
-public:
-    virtual void cgen() = 0;
-};
+    class ProgramNode : public Node {
+    private:
+        std::vector<Decl *> declerations;
+    public:
+        virtual void cgen() {};
 
-class ProgramNode : public Node {
-private:
-    std::vector<Decl *> declerations;
-public:
-    virtual void cgen() {};
-
-    virtual void addDecl(Decl *decl);
-};
-
+        virtual void addDecl(Decl *decl);
+    };
+}
 #endif //DECAFCOMPILER_NODE_H
